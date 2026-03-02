46c66bc5d1e7f23ed6d36da39112b94898095553
ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-03-02 22:22:52 +0530

    added player switching logic and active markers logic

--- game.js
+++ game.js
@@ -1,18 +1,52 @@
+//Game Board IIFE below
 const gameBoard = (function() {
     const markerSlots = Array(9).fill("");
 
     const getBoard = () => markerSlots;
+    const dropMarker = (index, marker) => {
+        if (markerSlots[index] === "") {
+            markerSlots[index] = marker;
+        } else {
+            console.log("Spot taken. Please choose another one.")
+        }
+    };
 
     return {
-        getBoard
+        getBoard,
+        dropMarker
      };
 
 
 })();
+//IIFE End
 
 const createPlayer = (name, marker) => {
     return { name, marker };
 }
 
-const player1 = createPlayer("Player1", "X");
-const player2 = createPlayer("Player2", "O");
+//Game Controller IIFE below
+const gameController = (function () {
+
+    const players = [
+    createPlayer("Player One", "X"),
+    createPlayer("Player Two", "Y")
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        if (activePlayer === players[0]) {
+            activePlayer = players[1];
+        } else {
+            activePlayer = players[0];
+        }
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    return {
+        switchPlayerTurn,
+        getActivePlayer
+    }
+
+})();
